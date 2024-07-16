d46f068dc06dc3a40396c6140ad0049b2f76bdbc
Dominique Clark <melaydominique@gmail.com>
2024-07-16 00:30:26 -0400

    Babel

--- frontend/src/components/fundraiser.tsx
+++ frontend/src/components/fundraiser.tsx
@@ -11,7 +11,7 @@ const Fundraiser: React.FC = () => {
                         {/* <h1 className='mt-1 font-semibold text-blue text-3xl'>For Brian Clark for Supervisor</h1> */}
                         <a href='https://www.eventbrite.com/e/darren-mccartys-comedy-dinner-fundraiser-for-brian-clark-for-supervisor-tickets-936482954727?aff=ebdsoporgprofile' target='_blank' rel='noopener noreferrer'>
                         <button className='py-2 px-4 mt-10 xl:mt-12 text-2xl xl:text-3xl bg-black text-white'>PURCHASE TICKETS</button></a>
-                        <h1 className='mt-12 xl:mt-16 font-semibold text-2xl'>Wednesday July 24 | South Lyon Hotel | 6pm est </h1>
+                        <h1 className='italic mt-12 xl:mt-16 font-semibold text-2xl'>Wednesday July 24 | South Lyon Hotel | 6pm est </h1>
         
                     </div>
                     <img src='/images/Dmac.jpg' alt='DMAC' className='w-1/4 h-auto'/>
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -15,15 +15,7 @@ const Navbar: React.FC = () => {
             </div>
         </div>
 
-    //   <div className='bg-blue h-10 flex items-center sticky top-0 z-50'>
-    //     <div className='flex flex-row ml-auto'>
-    //         <h1 className='mr-3 text-white font-semibold'>Purpose</h1>
-    //         <h1 className='mr-3 text-white font-semibold'>Plan</h1>
-    //         <h1 className='mr-3 text-white font-semibold'>About</h1>
-    //         <h1 className='mr-3 text-white font-semibold'>Contact</h1>
-    //         <h1 className='mr-3 text-white font-semibold'>Donate</h1>
-    //     </div>
-    //   </div>
+
 
     )
 }
