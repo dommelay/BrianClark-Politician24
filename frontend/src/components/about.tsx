c756068cbb314f9891209edb187652d81f6c8a09
Dominique Clark <melaydominique@gmail.com>
2024-07-17 09:54:31 -0400

    carousel, about, responsive

--- frontend/src/components/about.tsx
+++ frontend/src/components/about.tsx
@@ -10,25 +10,26 @@ const About: React.FC = () => {
     return (
         <div className=' bg-grey px-12 py-12'>
 
-            <div className='flex flex-col lg:flex-row justify-center '>
-                {/* <div className='block lg:hidden '>
-                    <div className='w-full md:w-1/2 lg:w-2/3 ml-4 max-w-25  md:mr-16 mr-10'>
-                        <img src='/images/solo.jpg' alt='Brian Headshot'></img>
+            <div className='flex flex-col lg:flex-row justify-center items-center '>
+             
+                    
+                    <div className=''>
+                        <h1 className='text-2xl sm:text-3xl  block lg:hidden font-bold'>Who Am I?</h1>
+                        <h1 className='text-xl sm:text-2xl block lg:hidden font-bold text-blue mb-6 italic'>A God fearing family man who makes many mistakes but believes in doing the right thing!</h1>
                     </div>
-                    <div className=''><h1 className='text-3xl lg:text-4xl font-bold'>Who Am I?</h1>
-                    <h1 className='text-2xl lg:mt-3 lg:text-3xl font-bold text-blue mb-6 italic'>A God fearing family man who makes many mistakes but believes in doing the right thing!</h1></div>
-                </div> */}
+          
 
-                <div className='w-full md:w-1/2 lg:w-2/3 ml-4 max-w-25  md:mr-16 mr-10'>
+                <div className='w-full  lg:w-2/3 lg:ml-4 max-w-25  xl:mr-16 lg:mr-10'>
                     <img src='/images/solo.jpg' alt='Brian Headshot'></img>
                 </div>
-                <div className='md:w-1/2 flex flex-col items-center'>
-                    <h1 className='text-3xl lg:text-4xl font-bold'>Who Am I?</h1>
-                    <h1 className='text-2xl lg:mt-3 lg:text-3xl font-bold text-blue mb-6 italic'>A God fearing family man who makes many mistakes but believes in doing the right thing!</h1>
-                    <p className=' font-semibold text-base xl:text-lg'>I’ll bring a unique blend of education and hands-on expertise to the role of Supervisor, while restoring your faith in local politcs. My wife and I successfully founded and operate our own insurance company, showcasing my entrepreneurial and managerial skills. I serve on the board of a leadership and personal development nonprofit, where I contribute to strategic planning and community outreach. </p><p className=' hidden lg:block font-semibold text-base xl:text-lg mt-3'>Additionally, I teach personal development and public speaking courses in high schools and have pioneered Reaching Higher classes in multiple jail facilities, partnering with Ignite. My dedication to fostering growth and leadership in individuals and businesses alike has been demonstrated through these varied roles. My comprehensive background in business, leadership, and community service equips me with the vision and experience needed to effectively serve and lead our township.</p>
+
+                <div className='w-full lg:w-1/2 flex flex-col items-center'>
+                    <h1 className='hidden lg:block text-3xl lg:text-4xl font-bold'>Who Am I?</h1>
+                    <h1 className='hidden lg:block text-2xl lg:mt-3 lg:text-3xl font-bold text-blue mb-6 italic'>A God fearing family man who makes many mistakes but believes in doing the right thing!</h1>
+                    <p className=' mt-3 lg:mt-0 font-semibold text-base xl:text-lg'>I’ll bring a unique blend of education and hands-on expertise to the role of Supervisor, while restoring your faith in local politcs. My wife and I successfully founded and operate our own insurance company, showcasing my entrepreneurial and managerial skills. I serve on the board of a leadership and personal development nonprofit, where I contribute to strategic planning and community outreach. </p><p className='  font-semibold text-base xl:text-lg mt-2'>Additionally, I teach personal development and public speaking courses in high schools and have pioneered Reaching Higher classes in multiple jail facilities, partnering with Ignite. My dedication to fostering growth and leadership in individuals and businesses alike has been demonstrated through these varied roles. My comprehensive background in business, leadership, and community service equips me with the vision and experience needed to effectively serve and lead our township.</p>
                   
                 </div>
-                <p className=' block lg:hidden font-semibold text-base xl:text-lg mt-3'>Additionally, I teach personal development and public speaking courses in high schools and have pioneered Reaching Higher classes in multiple jail facilities, partnering with Ignite. My dedication to fostering growth and leadership in individuals and businesses alike has been demonstrated through these varied roles. My comprehensive background in business, leadership, and community service equips me with the vision and experience needed to effectively serve and lead our township.</p>
+               
                   
             </div>
 
--- frontend/src/components/photocarousel.tsx
+++ frontend/src/components/photocarousel.tsx
@@ -8,10 +8,11 @@ const PhotoCarousel: React.FC = () => {
     return (
         
         <div className=' mb-10 flex flex-col items-center justify-center p-12'>
-            <h1 className='font-bold text-6xl italic text-blue mb-10'>Meet My Family</h1>
-            <div className="flex overflow-x-auto space-x-4 h-96 md:h-512">
+            <h1 className='font-bold text-4xl md:text-6xl italic text-blue mb-10'>Meet My Family</h1>
+            <div className="flex overflow-x-auto space-x-4 h-2/3 sm:h-96 md:h-512">
                 <img src="/images/fam1.jpg" alt="family photo 1" className="w-full rounded-lg" />
                 <img src="/images/fam2.jpg" alt="family photo 2" className="w-full rounded-lg" />
+                <img src="/images/Ricefam.jpg" alt="family photo 5" className="w-full  rounded-lg" />
                 <img src="/images/fam3.jpg" alt="family photo 3" className="w-full rounded-lg" />
                 <img src="/images/fam4.jpg" alt="family photo 4" className="w-full  rounded-lg" />
                 <img src="/images/fam5.jpg" alt="family photo 5" className="w-full  rounded-lg" />
