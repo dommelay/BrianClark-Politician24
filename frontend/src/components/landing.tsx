e9c0bf1e5ac066e6d4422b447899fec65e17af7c
Dominique Clark <melaydominique@gmail.com>
2024-07-17 10:21:41 -0400

    landing and nav fonts responsive, change responseive

--- frontend/src/components/landing.tsx
+++ frontend/src/components/landing.tsx
@@ -11,8 +11,8 @@ const Landing: React.FC = () => {
                 <img className='w-full h-full object-cover' src='/images/landing.jpg' alt='Brian Clarks Family'/>
 
                 <div className="absolute inset-0 flex flex-col items-center text-white bg-black bg-opacity-25">
-                    <h1 className='text-5xl md:text-7xl font-bold mt-4 xl:mt-7'>Brian Clark</h1>
-                    <h1 className='text-4xl md:text-6xl font-bold'>Superior Township Supervisor</h1>
+                    <h1 className='text-3xl md:text-6xl lg:text-7xl font-bold mt-0 sm:mt-4 xl:mt-7'>Brian Clark</h1>
+                    <h1 className='text-2xl md:text-5xl lg:text-6xl font-bold'>Superior Township Supervisor</h1>
                 </div>
 
                 <div className="absolute w-full bottom-0 text-center p-4">
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -21,15 +21,15 @@ const Navbar: React.FC = () => {
 
         <div id='nav' className="bg-blue h-12 md:h-16 flex items-center sticky top-0 z-50">
             <div className=''>
-                <Link to="landing" smooth={true} duration={500} className=" text-white font-semibold text:xl lg:text-3xl italic cursor-pointer ml-6" offset={handleOffset()}>Brian Clark</Link>
+                <Link to="landing" smooth={true} duration={500} className=" text-white font-semibold  sm:text-xl lg:text-3xl italic cursor-pointer ml-6" offset={handleOffset()}>Brian Clark</Link>
             </div>
             <div className="flex flex-row ml-auto">
-                <Link to="purpose" smooth={true} duration={500} className="mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>Purpose</Link>
-                <Link to="plan" smooth={true} duration={500} className="mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>Plan</Link>
-                <Link to="vision" smooth={true} duration={500} className="mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>Vision</Link>
-                <Link to="about" smooth={true} duration={500} className="mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>About</Link>
-                <Link to="contact" smooth={true} duration={500} className="mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>Contact</Link>
-                <a href='https://www.paypal.com/donate/?hosted_button_id=KNPM3W83MVMSN' target="_blank" rel="noopener noreferrer" className="mr-6 text-white font-semibold text-base md:text-lg lg:text-xl hover:underline">Donate</a>
+                <Link to="purpose" smooth={true} duration={500} className="mr-2 sm:mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>Purpose</Link>
+                <Link to="plan" smooth={true} duration={500} className="mr-2 sm:mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>Plan</Link>
+                <Link to="vision" smooth={true} duration={500} className="mr-2 sm:mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>Vision</Link>
+                <Link to="about" smooth={true} duration={500} className="mr-2 sm:mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>About</Link>
+                <Link to="contact" smooth={true} duration={500} className="mr-2 sm:mr-4 text-white font-semibold text-base md:text-lg lg:text-xl cursor-pointer hover:underline" offset={handleOffset()}>Contact</Link>
+                <a href='https://www.paypal.com/donate/?hosted_button_id=KNPM3W83MVMSN' target="_blank" rel="noopener noreferrer" className="mr-3 sm:mr-6 text-white font-semibold text-base md:text-lg lg:text-xl hover:underline">Donate</a>
             </div>
         </div>
 
